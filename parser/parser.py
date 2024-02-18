cc27904e8df03e4bd56ca7922c751e964b9c395e
Igareck99 <igor9.i@yandex.ru>
2024-02-18 23:33:15 +0300

    add parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,17 +1,35 @@
 import bs4
 import requests
-
-urls = ['https://www.scalemates.com/colors/ak-3rd-generation-afv--976']
+import urllib
+import os
+urls = ['https://www.scalemates.com/colors/italeri-acrylic-paint--678',
+        'https://www.scalemates.com/colors/ak-3rd-generation-afv--976',
+        'https://www.scalemates.com/colors/revell-aqua-color--654']
 
 def getHtml(url: str):
     data = requests.get(url)
-    print(data)
+    print('Current url')
     if data.status_code == 200:
-        soup = bs4.BeautifulSoup(data.text, "html.parser")
-        print(soup)
-
+        try:
+            soup = bs4.BeautifulSoup(data.text, "html.parser")
+            allNews = soup.findAll('div', class_= 'ac dg bgl cc pr mt4')
+            for product in allNews:
+                product_name = product.find('span', class_='bgb nw').text
+                series_info = product.find('div', class_='ut').text
+                finish_type = product.find('div', class_='ccf center dib nw bgn').text
+                paint_type = product.find('div', class_='cct center dib nw bgb').text
+                style_attribute = product.find('div', class_='pr dib')['style'].split(':')[-1]
+                print(f"Product Name: {product_name}")
+                print(f"Series Info: {series_info}")
+                print(f"Finish Type: {finish_type}")
+                print(f"Paint Type: {paint_type}")
+                print(f"Color Name: {style_attribute}")
+                print('\n\n')
+        except:
+            pass
 def run():
     for x in urls:
         getHtml(x)
 
 run()
+
